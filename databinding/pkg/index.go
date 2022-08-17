368d57f47330c23fd724bbea5ebccf332a32313e
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2022-08-17 19:44:06 +0530

    adds json marshal function

--- databinding/pkg/index.go
+++ databinding/pkg/index.go
@@ -1,6 +1,16 @@
 package pkg
 
-// 
-func JSONMarshal() {
-	
+import "encoding/json"
+
+// JSONMarshal
+// JSON encoding
+func JSONMarshal(instance interface{}) (bytes []byte, err error) {
+	// Marshal
+	bytes, err = json.Marshal(instance)
+	if err != nil {
+		return nil, err
+	}
+
+	// Returns
+	return bytes, nil
 }
