a76905f27fb9159d205f3051edc1f817a53166a0
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2022-08-18 23:57:21 +0530

    adds test file for json marshal & unmarshal

--- databinding/main.go
+++ databinding/main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"log"
+
+	dataBindingPkg "golang-examples/databinding/pkg"
+)
+
+type testStruct struct {
+	Language string  `json:"language"`
+	Version  float64 `json:"version"`
+}
+
 // Package testing file
 func main() {
+	// Defines map & assign value
+	testMap := map[string]interface{}{
+		"language": "golang",
+		"version":  1.16,
+	}
+
+	// Lets bind testMap data into struct
+	// Firstly we have to do json encode
+	bytes, err := dataBindingPkg.JSONMarshal(testMap)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Now unmarshal json encoded data into struct
+	tstruct := testStruct{}
+	err = dataBindingPkg.JSONUnmarshal(bytes, &tstruct)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
