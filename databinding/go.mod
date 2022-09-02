aa4056fe6cb6eeb1860c39e8234e0c2ce872431a
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2022-09-03 01:02:10 +0530

    adds bson unmarshal

--- databinding/go.mod
+++ databinding/go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/lokesh-go/golang-examples/print v0.0.0-20220821162541-cfcd0ef00014
 	github.com/mitchellh/mapstructure v1.5.0
+	go.mongodb.org/mongo-driver v1.10.1
 )
--- databinding/pkg/index.go
+++ databinding/pkg/index.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/mitchellh/mapstructure"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // JSON encode
@@ -22,6 +23,31 @@ func JSONMarshal(instance interface{}) (bytes []byte, err error) {
 // pointed to by instance
 func JSONUnmarshal(bytes []byte, instance interface{}) (err error) {
 	err = json.Unmarshal(bytes, instance)
+	if err != nil {
+		return err
+	}
+
+	// Returns
+	return nil
+}
+
+// BSON encode
+func BSONMarshal(instance interface{}) (bytes []byte, err error) {
+	// Marshal
+	bytes, err = bson.Marshal(instance)
+	if err != nil {
+		return nil, err
+	}
+
+	// Returns
+	return bytes, nil
+}
+
+// Unmarshal parses the BSON-encoded data and stores the result in the value
+// pointed to by instance
+func BSONUnmarshal(bytes []byte, instance interface{}) (err error) {
+	// Unmarshal
+	err = bson.Unmarshal(bytes, instance)
 	if err != nil {
 		return err
 	}
