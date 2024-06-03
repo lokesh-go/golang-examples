0fb85b8d868b5b0cdf58a9f53e8bcaa5272328ac
Lokesh Chandra <lokeshchandra@Lokeshs-Mac-mini.local>
2024-06-03 19:31:45 +0530

    added sayhello

---
+++ goroutines/sayhello/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+Notes: Go follows a model of concurrency called the FORK-JOIN model.
+Fork - Split off a child branch of execution to be run concurrently with its parent.
+Join - At some point in the future, these concurrent branches of execution will join back together.
+Where the child rejoins the parent is called a JOIN POINT.
+*/
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Creates child branch
+	go sayHello(&wg)
+
+	// This is JOIN POINT
+	wg.Wait()
+}
+
+func sayHello(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("hello")
+}
