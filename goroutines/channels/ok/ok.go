7d5060d3595a9fb4fc0c5055431cd7ec1d93850e
lokesh.chandra <lokesh.chandra525@gmail.com>
2024-06-18 23:01:05 +0530

    added example for close channel

--- goroutines/channels/ok/ok.go
+++ goroutines/channels/ok/ok.go
@@ -15,4 +15,17 @@ func Example() {
 	// Reading from channel with two parameters
 	m, ok := <-msg
 	fmt.Println(m, ok)
+
+	// Create channel
+	stream := make(chan int)
+
+	// Close channel to see the result
+	close(stream)
+
+	// Reading from channel with two parameters
+	s, ok := <-stream
+
+	// ok variable --- is false, indicating that the value we received is the zero
+	// value for int, or 0 and not a value placed on the stream
+	fmt.Println(s, ok)
 }
