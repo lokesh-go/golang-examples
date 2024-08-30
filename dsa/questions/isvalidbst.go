8fb66c8689e6dba6a697dd4d2c129c17a2d06f73
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-08-30 21:51:19 +0530

    added validate binary search tree

---
+++ dsa/questions/isvalidbst.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+	return isBST(root, math.MinInt, math.MaxInt)
+}
+
+func isBST(node *TreeNode, min int, max int) bool {
+	if node == nil {
+		return true
+	}
+	if node.Val >= max || node.Val <= min {
+		return false
+	}
+	return isBST(node.Left, min, node.Val) && isBST(node.Right, node.Val, max)
+}
