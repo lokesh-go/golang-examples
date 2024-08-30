5ccbd69c9cf9bae2cbb6d81339b730b565b3a004
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-08-30 15:53:50 +0530

    added find minimum in rotated sorted array

--- dsa/questions/findmininrotatedsortedarray.go
+++ dsa/questions/findmininrotatedsortedarray.go
@@ -1,1 +1,20 @@
 package main
+
+import "math"
+
+func findMin(nums []int) int {
+	minNum := math.MaxInt
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[low] <= nums[mid] {
+			minNum = min(minNum, nums[low])
+			low = mid + 1
+		} else {
+			minNum = min(minNum, nums[mid])
+			high = mid - 1
+		}
+	}
+	return minNum
+}
