3a85d393e3c1686b0290414b8ee69216f73b0315
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-08-30 22:32:58 +0530

    added kth smallest element in a bst - inorder

---
+++ dsa/questions/kthsmallestnuminbst.go
@@ -0,0 +1,33 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func kthSmallest(root *TreeNode, k int) int {
+	return inOrder(root, &k)
+}
+
+func inOrder(root *TreeNode, k *int) int {
+	if root == nil {
+		return -1
+	}
+	leftRes := inOrder(root.Left, k)
+	if leftRes != -1 {
+		return leftRes
+	}
+	*k--
+	if *k == 0 {
+		return root.Val
+	}
+	rightRes := inOrder(root.Right, k)
+	if rightRes != -1 {
+		return rightRes
+	}
+	return -1
+}
