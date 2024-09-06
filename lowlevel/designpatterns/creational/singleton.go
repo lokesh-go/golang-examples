38d0490a082199a6a55dd72e8a4478adf102ac3e
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-09-06 20:48:16 +0530

    added singleton creational design pattern

---
+++ lowlevel/designpatterns/creational/singleton.go
@@ -0,0 +1,43 @@
+package creational
+
+import (
+	"fmt"
+	"sync"
+)
+
+type single struct{}
+
+// Global instance
+var singleInstance *single
+var lock sync.Mutex
+var once sync.Once
+
+func getInstance() *single {
+	if singleInstance == nil {
+		// Only lock when checking and creating the instance
+		lock.Lock()
+		defer lock.Unlock()
+
+		// Double-check if instance is still nil after acquiring the lock
+		if singleInstance == nil {
+			fmt.Println("Creating a single instance")
+			singleInstance = &single{}
+		} else {
+			fmt.Println("Single instance created")
+		}
+	} else {
+		fmt.Println("Single instance already created")
+	}
+	return singleInstance
+}
+
+func getInstanceWithOnce() *single {
+	// Thread-safe getInstance using sync.Once
+	once.Do(func() {
+		fmt.Println("Creating a single instance")
+		singleInstance = &single{}
+	})
+
+	fmt.Println("Single instance already created")
+	return singleInstance
+}
