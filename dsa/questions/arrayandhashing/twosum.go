fc4638379a839140e1724d9235aa31485fecf696
lokesh.chandra <lokesh.chandra525@gmail.com>
2024-10-06 22:02:20 +0530

    added comments

--- dsa/questions/arrayandhashing/twosum.go
+++ dsa/questions/arrayandhashing/twosum.go
@@ -1,5 +1,13 @@
 package main
 
+/*
+	1. Initialise empty hash map
+	2. Range over numbers
+	3. Find remaining num (target-num) in hash map
+		- If found return (ind of 1st num, ind of 2nd num)
+		- else hashMap[num] = ind
+*/
+
 func twoSum(nums []int, target int) []int {
 	hashMap := map[int]int{}
 	for ind, num := range nums {
