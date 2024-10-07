585b9d3bc305b08d2f0d207cf9175fb349e7dc5a
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-07 11:11:46 +0530

    added comments

--- dsa/questions/arrayandhashing/productofarray.go
+++ dsa/questions/arrayandhashing/productofarray.go
@@ -1,5 +1,20 @@
 package main
 
+/*
+	The trickier part is when array contains 0
+	1. Counts the zero & calculate the product of nums in the array (if num is 0 don't multiply just continue)
+	2. if zeroCount > 1
+		return array with all 0 nums -> make([]int, len(nums))
+	3. Forms res
+		- If zeroCount == 1
+			- Case 1:
+					num is 0 -> append(res, product)
+			- Case 2:
+					rest of element will be 0
+		else
+			- res = append(res, product / num)
+*/
+
 func productExceptSelf(nums []int) []int {
 	product := int(1)
 	zeroCount := 0
