84a8d673b0e5a54123ec0e2e900805bf3888251a
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-07 12:59:30 +0530

    added comments

--- dsa/questions/twopointers/threesum.go
+++ dsa/questions/twopointers/threesum.go
@@ -1,5 +1,26 @@
 package twopointers
 
+/*
+	1. Sort the array
+	2. Loop over the nums array
+		- Avoid duplicate numbers
+			- if i > 0 && nums[i] == nums[i-1]; continue
+	3. Take two pointers (j = i+1 , k = len(nums)-1)
+	4. for (j < k); check
+		- if sum := num + nums[j] + nums[k] == 0
+			- append into result array if it is not in hashMap to avoid duplicate result (key - (1,2,3))
+			- j++
+			- k--
+			- To avoid duplicate numbers
+				- Skip util j < k -> if nums[j] == nums[j-1]; j++ else break
+			- Same for K
+				- Skip util j<k -> if nums[k] == nums[k+1]; k-- else break
+		- else if sum < 0
+			- j++
+		- else if sum > 0
+			- k--
+*/
+
 import (
 	"sort"
 	"strconv"
