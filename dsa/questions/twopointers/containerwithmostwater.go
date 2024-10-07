a2cb39865870d9b829a4169ce39ec0f120376779
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-07 13:27:03 +0530

    added comments

--- dsa/questions/twopointers/containerwithmostwater.go
+++ dsa/questions/twopointers/containerwithmostwater.go
@@ -1,5 +1,18 @@
 package twopointers
 
+/*
+	1. Take two pointers (left = 0, right = len(height)-1)
+	2. Loop util left < right
+		- containerWid := right - left
+		- containerLen := min(height[left], height[right])
+		- containerArea := containerWidth * containerLen
+		- area = max(area, containerArea)
+		- if height of left is less than right
+			- left++
+		else
+			- right--
+*/
+
 func maxArea(height []int) int {
 	var area int
 	left := 0
