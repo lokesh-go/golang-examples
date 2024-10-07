97a5e0c4944e5db57df34e605a931e87a54dbdc2
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-07 16:22:15 +0530

    added comments

--- dsa/questions/slidingwindow/longestsubstringwithoutrepeatchar.go
+++ dsa/questions/slidingwindow/longestsubstringwithoutrepeatchar.go
@@ -1,5 +1,18 @@
 package slidingwindow
 
+/*
+	Calculate length in a window util we found duplicates
+
+	loop over string
+	assign first index to the left pointer
+	from every left pointer to the end of string, we start a window
+		- maintain hashMap for this (to check for particular windows char is duplicate or not)
+		- If duplicate found then break
+		- increase length and left pointer
+	- After each window we calculate the max length
+	return length
+*/
+
 func lengthOfLongestSubstring(s string) int {
 	var longest int
 	for ind := range s {
