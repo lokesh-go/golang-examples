0647d1e12bae60eef53ebb3744a3fd4154996c90
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-07 15:11:31 +0530

    added comments

--- dsa/questions/slidingwindow/besttimetobuysellstock.go
+++ dsa/questions/slidingwindow/besttimetobuysellstock.go
@@ -1,5 +1,19 @@
 package slidingwindow
 
+/*
+	1. Take right & left two pointers (left = 0, right = left + 1)
+	2. Loop right < len(prices)
+		If prices is less than tomorrow then will prefer to buy and star calculating the profit
+		- if prices[left] < prices[right]
+			Calculate profit = prices[right] - prices[left]
+			maxProfit = max(maxProfit, profit)
+		else
+			Otherwise move forward left with right
+			left = right
+
+		right++
+*/
+
 func maxProfit(prices []int) int {
 	maxProfit := 0
 	left := 0
@@ -9,11 +23,8 @@ func maxProfit(prices []int) int {
 			profit := prices[right] - prices[left]
 			maxProfit = max(maxProfit, profit)
 		} else {
-			// Buy
-			// Because price is low so will buy here
 			left = right
 		}
-		// Sell
 		right++
 	}
 	return maxProfit
