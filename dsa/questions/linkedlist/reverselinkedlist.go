d2c64db557696b6a9dfc1606e72fa300105e3197
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-08 13:29:03 +0530

    added reverse linked list

---
+++ dsa/questions/linkedlist/reverselinkedlist.go
@@ -0,0 +1,18 @@
+package linkedlist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) (prev *ListNode) {
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+	return prev
+}
