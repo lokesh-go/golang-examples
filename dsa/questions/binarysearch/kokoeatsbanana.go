1ee06c777e8dc48c9f6b94149b7bdb1b8a936413
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2024-10-08 12:39:35 +0530

    added comments

--- dsa/questions/binarysearch/kokoeatsbanana.go
+++ dsa/questions/binarysearch/kokoeatsbanana.go
@@ -1,5 +1,22 @@
 package binarysearch
 
+/*
+	For the bruteforce solution
+	Let's say It has started with 1 banana per hour
+	and continuously it will try with 1, 2, 3, 4, 5, 6, 7, ......... -> but what is maximum number here ?
+
+	in the piles array we can get maximum numbers
+	suppose - piles = [3,6,7,11]
+		- get the max number -> 11
+		Now we can check one by one from 1 to 11
+		1,2,3,4,...........11
+
+	To optimising this we can implement binary search for picking a number from 1,2,3,4,......11 (this is in sorted order)
+		- minSpeed is - 1
+		- maxSpeed is max number of that array - 11
+	and just calculate total hours to each banana
+*/
+
 import "math"
 
 func minEatingSpeed(piles []int, h int) int {
